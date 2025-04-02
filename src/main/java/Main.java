277c2aa4f28bf4c6a9c1e7bdd427fd4d607aa3dd
DanielZornek <daniel_zornek91@outlook.com>
2025-04-02 19:47:34 -0300

    titulos dos posts mapeados | desafio concluido

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -16,33 +15,40 @@ public class Main {
         // desserializando o json para um objeto post
         List<Post> posts = g.fromJson(dadosRequisicao, new TypeToken<List<Post>>(){}.getType());
 
-//        for(int i = 0; i<posts.toArray().length; i++){
-//            System.out.println(posts.get(i));
-//        }
-//        List<Post> postsComNihil = posts.stream()
-//                .filter(p -> p.getBody().contains("nihil"))
-//                .toList();
-//        System.out.println("Todos os post com Nihil\n");
-//        postsComNihil.forEach(System.out::println);
+        for(int i = 0; i<posts.toArray().length; i++){
+            System.out.println(posts.get(i));
+        }
+        List<Post> postsComNihil = posts.stream()
+                .filter(p -> p.getBody().contains("nihil"))
+                .toList();
+        System.out.println("Todos os post com Nihil\n");
+        postsComNihil.forEach(System.out::println);
 
-//        List<Post> postsComNihilOrdenado = posts.stream()
-//                .filter(post -> post.getBody().contains("nihil"))
-//                .sorted(Comparator.comparingInt(Post::getId))
-//                .toList();
-//        System.out.println("Todos os posts com Nihil ordenados por id: \b");
-//        postsComNihilOrdenado.forEach(System.out::println);
+        List<Post> postsComNihilOrdenado = posts.stream()
+                .filter(post -> post.getBody().contains("nihil"))
+                .sorted(Comparator.comparingInt(Post::getId))
+                .toList();
+        System.out.println("Todos os posts com Nihil ordenados por id: \b");
+        postsComNihilOrdenado.forEach(System.out::println);
 
-//        List<Integer> listaUsuarios = posts.stream()
-//                .map(Post::getUserId)
-//                .distinct()
-//                .toList();
-//
-//        listaUsuarios.forEach(userId ->{
-//            List<Post> postsDoUsuário = posts.stream()
-//                    .filter(post -> post.getUserId() == userId)
-//                    .toList();
-//            System.out.println("Identificador do usuário: "+userId+"\n");
-//            postsDoUsuário.forEach(System.out::println);
-//        });
+        List<Integer> listaUsuarios = posts.stream()
+                .map(Post::getUserId)
+                .distinct()
+                .toList();
+
+        listaUsuarios.forEach(userId ->{
+            List<Post> postsDoUsuário = posts.stream()
+                    .filter(post -> post.getUserId() == userId)
+                    .toList();
+            System.out.println("Identificador do usuário: "+userId+"\n");
+            postsDoUsuário.forEach(System.out::println);
+        });
+
+        List<String> titulosPostsFiltrados = postsComNihil.stream()
+                        .map(Post::getTitle)
+                                .toList();
+
+        System.out.println("Títulos dos posts filtrados (que contenham Nihil)");
+        titulosPostsFiltrados.forEach(System.out::println);
     }
 }
