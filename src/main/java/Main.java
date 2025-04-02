b4cd9abf342540e8c44e1ba044e15013c2640e07
DanielZornek <daniel_zornek91@outlook.com>
2025-04-02 19:05:51 -0300

    modelo post criado | primeiro desafio com stream feito | filtro de posts com nihil

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
+import br.edu.fatecpg.tecprog.model.Post;
 import br.edu.fatecpg.tecprog.service.ConsumoAPI;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         String dadosRequisicao = ConsumoAPI.obterDados("https://jsonplaceholder.typicode.com/posts");
 
-        System.out.println(dadosRequisicao);
+        Gson g = new Gson();
+        // desserializando o json para um objeto post
+        List<Post> posts = g.fromJson(dadosRequisicao, new TypeToken<List<Post>>(){}.getType());
+
+//        for(int i = 0; i<posts.toArray().length; i++){
+//            System.out.println(posts.get(i));
+//        }
+        List<Post> postsComQui = posts.stream()
+                .filter(p -> p.getBody().contains("nihil"))
+                .toList();
+        System.out.println("Todos os post com Nihil\n");
+        postsComQui.forEach(System.out::println);
     }
 }
